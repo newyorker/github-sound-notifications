eaad7209ae85a8394e410df76cded9967dfe3aab
rememberlenny <leonard_bogdonoff@condenast.com>
2014-12-15 18:23:13 -0500

    Disable csrf

--- app/controllers/listen_controller.rb
+++ app/controllers/listen_controller.rb
@@ -1,7 +1,8 @@
 class ListenController < ApplicationController
 
   def receive
-    render text: "Success"
+    # puts request.body.read
+    render text: "Thanks for sending a POST request with cURL! Payload: #{request.body.read}"
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  post '/listen' => 'listen#receive'
+  match '/listen' => 'listen#receive', via: :post
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
