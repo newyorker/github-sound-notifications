3c423dad32a4edd1a1f92c6a5bc08577423dfad8
rememberlenny <leonard_bogdonoff@condenast.com>
2014-12-15 18:14:51 -0500

    Create the listener

---
+++ app/controllers/listen_controller.rb
@@ -0,0 +1,7 @@
+class ListenController < ApplicationController
+
+  def receive
+
+  end
+
+end
---
+++ spec/controllers/listen_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe ListenController, :type => :controller do
+
+end
