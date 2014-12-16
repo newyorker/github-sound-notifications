5356813266b04884031caff25aecf808c93e2ba0
rememberlenny <leonard_bogdonoff@condenast.com>
2014-12-16 17:32:58 -0500

    Correct the json object

--- app/controllers/listen_controller.rb
+++ app/controllers/listen_controller.rb
@@ -14,10 +14,10 @@ class ListenController < ApplicationController
 
   def update
     json_output = {}
-    json_output => :count = Record.count
-    json_output => :last_payload = Record.last.payload
+    json_output["count"] = Record.count
+    json_output["last_payload"] = Record.last.payload
     if Record.last.comment != nil 
-      json_output => :last_comment = Record.last.comment
+      json_output["last_comment"] = Record.last.comment
     end 
     render json: json_output
   end
