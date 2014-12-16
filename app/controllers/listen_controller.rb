bf955887bac746241b2edb3d3bd9a9a64de6ed9a
rememberlenny <leonard_bogdonoff@condenast.com>
2014-12-16 17:27:43 -0500

    Save the comment body

--- app/controllers/listen_controller.rb
+++ app/controllers/listen_controller.rb
@@ -2,13 +2,23 @@ class ListenController < ApplicationController
 
   def receive
     # puts request.body.read
-    render text: "Thanks for sending a POST request with cURL! Payload: #{request.body.read}"
-    r = Record.new(payload: request.headers['X-GitHub-Event']);
+    event = request.headers['X-GitHub-Event']
+    body = request.body.read
+    render text: "Thanks for sending a POST request with cURL! Payload: #{body}"
+    r = Record.new(payload: event)
+    if event == 'issue_comment'
+      r.comment = body.comment.body
+    end
     r.save
   end
 
   def update
-    render json: {"count" => Record.count, "last_payload" => Record.last.payload}
+    json_output["count"] = Record.count
+    json_output["last_payload"] = Record.last.payload
+    if Record.last.comment != nil 
+      json_output["last_comment"] = Record.last.comment
+    end 
+    render json: json_output
   end
 
 end
