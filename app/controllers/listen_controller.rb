a13d4c23df6eee062d11af466cc7a1cb2ce868cc
rememberlenny <leonard_bogdonoff@condenast.com>
2014-12-16 17:30:10 -0500

    Fix the output

--- app/controllers/listen_controller.rb
+++ app/controllers/listen_controller.rb
@@ -13,10 +13,11 @@ class ListenController < ApplicationController
   end
 
   def update
-    json_output["count"] = Record.count
-    json_output["last_payload"] = Record.last.payload
+    json_output = {}
+    json_output => :count = Record.count
+    json_output => :last_payload = Record.last.payload
     if Record.last.comment != nil 
-      json_output["last_comment"] = Record.last.comment
+      json_output => :last_comment = Record.last.comment
     end 
     render json: json_output
   end
