9fa084b254792ec99b2023d7fe24c4d6c828ed28
rememberlenny <leonard_bogdonoff@condenast.com>
2014-12-16 17:54:35 -0500

    Save the comments

--- app/controllers/listen_controller.rb
+++ app/controllers/listen_controller.rb
@@ -4,10 +4,10 @@ class ListenController < ApplicationController
     # puts request.body.read
     event = request.headers['X-GitHub-Event']
     payload = request.body.read
-    comment = payload.to_json
     if event == 'issue_comment'
-      render text: "Thanks for sending a POST request with cURL! Comment: #{comment.comment.body}"
-      r = Record.new(payload: event, comment: comment.comment.body)
+      puts payload['comment']
+      render text: "Thanks for sending a POST request with cURL! Comment:"
+      # r = Record.new(payload: event, comment: payload.comment.body)
     else
       render text: "Thanks for sending a POST request with cURL! Payload: #{payload}"
       r = Record.new(payload: event)
